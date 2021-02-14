8f88ce1fab9a135ee8599dc320a5b4e3751f8125
William Fox <wfox96@gmail.com>
2021-02-13 20:02:02 -0800

    function rename

--- tests/bot/test_utils.py
+++ tests/bot/test_utils.py
@@ -12,20 +12,20 @@ class Test(TestCase):
         failure_test_cases = ['i really want to go to the store', "rimworld", "gorilla", "king kong", "i can't a",
                               'busy', "can't tonight"]
         for success_test_case in success_test_cases:
-            self.assertTrue(utils.is_message_overwatch_time(success_test_case))
+            self.assertTrue(utils.is_message_overwatch_time_basic(success_test_case))
 
         for failure_test_case in failure_test_cases:
-            self.assertFalse(utils.is_message_overwatch_time(failure_test_case))
+            self.assertFalse(utils.is_message_overwatch_time_basic(failure_test_case))
 
     def test__is_long_form_overwatch_time(self):
         success_test_cases = ['i a in 20 minutes', 'i can a for 45 mins', 'i can a in 7']
         failure_test_cases = ['i really want to go to the store', "rimworld", "gorilla", "king kong", "i can't a",
                               'busy', "can't tonight"]
         for success_test_case in success_test_cases:
-            self.assertTrue(utils._is_long_form_overwatch_time(success_test_case))
+            self.assertTrue(utils.is_message_overwatch_time_linear_regression(success_test_case))
 
         for failure_test_case in failure_test_cases:
-            self.assertFalse(utils._is_long_form_overwatch_time(failure_test_case))
+            self.assertFalse(utils.is_message_overwatch_time_linear_regression(failure_test_case))
 
     def test_get_clean_message_content(self):
         test_strings = {'```a```': '', 'a': 'a', '`a`': '', "```python print('Hello World!')```": ''}
