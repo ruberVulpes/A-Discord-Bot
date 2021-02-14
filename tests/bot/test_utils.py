1a3ea3c96dc3766bbf04f867affe2348f63f97df
William Fox <wfox96@gmail.com>
2021-02-13 21:47:25 -0800

    fixed model tests

--- tests/bot/test_utils.py
+++ tests/bot/test_utils.py
@@ -18,16 +18,14 @@ class Test(TestCase):
             self.assertFalse(utils.is_message_overwatch_time_basic(failure_test_case))
 
     def test_is_message_overwatch_time_linear_regression(self):
-        # TODO: Update to use ML Model with expected good/bad
-        self.assertTrue(True)
-        # success_test_cases = ['i a in 20 minutes', 'i can a for 45 mins', 'i can a in 7']
-        # failure_test_cases = ['i really want to go to the store', "rimworld", "gorilla", "king kong", "i can't a",
-        #                       'busy', "can't tonight"]
-        # for success_test_case in success_test_cases:
-        #     self.assertTrue(utils.is_message_overwatch_time_linear_regression(success_test_case))
-        #
-        # for failure_test_case in failure_test_cases:
-        #     self.assertFalse(utils.is_message_overwatch_time_linear_regression(failure_test_case))
+        success_test_cases = ['i a in 20 minutes', 'i can a for 45 mins', 'i can a in 7']
+        failure_test_cases = ['i really want to go to the store', "rimworld", "gorilla", "king kong", "i can not a",
+                              'busy', "can not tonight"]
+        for success_test_case in success_test_cases:
+            self.assertTrue(utils.is_message_overwatch_time_linear_regression(success_test_case))
+
+        for failure_test_case in failure_test_cases:
+            self.assertFalse(utils.is_message_overwatch_time_linear_regression(failure_test_case))
 
     def test_get_clean_message_content(self):
         test_strings = {'```a```': '', 'a': 'a', '`a`': '', "```python print('Hello World!')```": '',
