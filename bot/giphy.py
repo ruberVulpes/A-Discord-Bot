f3a1bfcf36d982cd5ffd0ba6f18baf1a424ae56a
William Fox <wfox96@gmail.com>
2021-02-16 18:32:11 -0800

    lock random gifs at pg13

--- bot/giphy.py
+++ bot/giphy.py
@@ -11,4 +11,5 @@ giphy = giphy_client.DefaultApi()
 def get_reaction_gif(query_strings: Optional[List[str]] = None) -> str:
     query_strings = query_strings or ['overwatch', 'gorillas', 'dance']
     query_string = random.choice(query_strings)
-    return random.choice(giphy.gifs_search_get(api_key=giphy_token, q=query_string, lang='en').data).bitly_url
+    search_result = giphy.gifs_search_get(api_key=giphy_token, q=query_string, lang='en', rating='pg-13')
+    return random.choice(search_result.data).bitly_url
