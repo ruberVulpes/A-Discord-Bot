defac09b8734e72200a75f47abf2069dd3a762da
William Fox <wfox96@gmail.com>
2021-02-20 12:12:16 -0800

    Add Database for Anti Spam (#3)
    * ++ db reqs
    
    * ++ db url
    
    * init db
    
    * using db for anti spam

--- env.py
+++ env.py
@@ -3,3 +3,6 @@ import os
 discord_token = os.environ['DISCORD_TOKEN']
 
 giphy_token = os.environ['GIPHY_TOKEN']
+
+# Use sqlite memory db for tests
+database_url = os.getenv('DATABASE_URL', 'sqlite:///:memory:')
