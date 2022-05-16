f39153c20440f3fdd473b78d35e72f9aea2b4930
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-16 13:52:39 +0800

    Delete tests directory

--- tests/db/test_models.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from db.models import MessageHistory
-
-
-class TestMessageHistory(TestCase):
-    def test_repr(self):
-        self.assertTrue(isinstance(MessageHistory(guild_id=1234, channel_id=1234).__repr__(), str))
