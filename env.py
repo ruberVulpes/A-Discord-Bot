8fd8c52354adb43a2f653cf550cf5e150ad01537
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-16 14:04:40 +0800

    Delete env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-discord_token = os.environ['DISCORD_TOKEN']
-
-giphy_token = os.environ['GIPHY_TOKEN']
-
-# Use sqlite memory db for tests
-database_url = os.getenv('DATABASE_URL', 'sqlite:///:memory:')
