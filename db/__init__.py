22488298bfddc1c471ada79155ad067b383f674c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-16 13:51:25 +0800

    Delete db directory

--- db/__init__.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from env import database_url
-
-engine = create_engine(database_url)
-session = sessionmaker(bind=engine)()
